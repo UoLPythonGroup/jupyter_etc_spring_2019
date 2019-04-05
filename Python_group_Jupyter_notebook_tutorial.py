acbfe742ee7ae493f67e2b139e347bf71c8f1513
Ruth <R.M.Holland@leeds.ac.uk>
2019-04-05 12:57:54 +0100

    Add rubbish ipython notebook
    This is what we did in the Python group session 5/4/19

---
+++ Python_group_Jupyter_notebook_tutorial.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+fish = 10
+a = 20
+
+
+# I can write some *text* which is **important** and can even have some maths, like $\alpha = \int x^2 dx $
+
+# # Main Title
+
+# ## Subtitle
+
+# ### Smaller title
+
+# ###### last subsection
+
+# In[3]:
+
+
+a
+
+
+# In[6]:
+
+
+def cubed(n):
+    a = n**3
+    return(a)
+
+
+# In[7]:
+
+
+cubed(10)
+
